4537eb554ed862c150a445e8ad146448db7e6c56
laiyiju <137786746+laiyiju@users.noreply.github.com>
2023-08-01 22:06:27 +0900

    Add files via upload

--- CH 08 Character Input-Output and Input Validation/C08_01.c
+++ CH 08 Character Input-Output and Input Validation/C08_01.c
@@ -1,1 +1,15 @@
+// 1. Devise a program that counts the number of characters in its input up to the end of file.
 
+#include<stdio.h>
+int main(void)
+{
+    int ch;         // declare ch as a int type, because char type cannot represent EOF character
+    int count = 0;
+
+    printf("Enter some characters:\n");
+    while ((ch = getchar()) != EOF)
+        count++;
+    printf("The text contains %d characters.\n", count);
+
+    return 0;
+}
