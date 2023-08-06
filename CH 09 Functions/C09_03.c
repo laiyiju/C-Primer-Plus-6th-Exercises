4299ab6781f4ab4876fc41a275919d87d196ec54
laiyiju <137786746+laiyiju@users.noreply.github.com>
2023-08-07 00:25:21 +0900

    Add files via upload

---
+++ CH 09 Functions/C09_03.c
@@ -0,0 +1,36 @@
+// 3. Write a function that takes three arguments: a character and two integers. The character
+//    is to be printed. The first integer specifies the number of times that the character is to
+//    be printed on a line, and the second integer specifies the number of lines that are to be
+//    printed. Write a program that makes use of this function.
+
+#include<stdio.h>
+
+void chrocol(char, int, int);   // character-row-column
+int main(void)
+{
+    char ch;
+    int col, row;
+
+    printf("Enter a printable character:\n");      // read character
+    while ((ch = getchar()) <= 32)     // if the input is ASCII unprintable characters
+        printf("Your input is not printable. Please try re-enter:\n");
+    
+    printf("Enter the numbers of columns and rows:\n"); 
+    while ((scanf("%d %d", &col, &row)) != 2)
+        printf("Invalid input. Please try re-enter:\n");
+    
+    chrocol(ch, col, row);
+
+    return 0;
+}
+
+void chrocol(char ch, int col, int row)
+{
+    int n, m;
+    for (n = 0; n < row; n++)
+    {
+        for (m = 0; m < col; m++)
+            putchar(ch);
+        printf("\n");
+    }
+}
