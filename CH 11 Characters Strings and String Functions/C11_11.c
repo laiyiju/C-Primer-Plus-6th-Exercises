1c8fa2b6d488f62225c9ff49895c8e4fe42fabd9
頼以居 <137786746+laiyiju@users.noreply.github.com>
2023-09-21 15:24:46 +0900

    Add files via upload

--- CH 11 Characters Strings and String Functions/C11_11.c
+++ CH 11 Characters Strings and String Functions/C11_11.c
@@ -7,16 +7,17 @@
 
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 #define ROW 10
 #define COL 40
 
 char *s_gets(char *st, int n);
 int menu(void);
 
-void print_original_strings(const char *st, int n);
-void print_ASCII_order(const char *st, int n);
-void print_increasing_length_order(const char *st, int n);
-void print_first_word_length_order(const char *st, int n);
+void print_original_strings(const char st[][COL], int n);
+void print_ASCII_order(const char st[][COL], int n);
+void print_increasing_length_order(const char st[][COL], int n);
+void print_first_word_length_order(const char st[][COL], int n);
 
 int main(void)
 {
@@ -25,7 +26,7 @@ int main(void)
     int choice;            // and an integer to store user's choice.
 
     puts("Enter up to 10 strings:");    // promtp for input
-    while (i <= 10 && (s_gets(array[i], COL)) != EOF)    // use a while loop(i<10) to read in strings
+    while (i <= 10 && (s_gets(array[i], COL)) != NULL)    // use a while loop(i<10) to read in strings
         i++;
 
     puts("Make one choice from the menu:");     // print menu;
@@ -35,7 +36,7 @@ int main(void)
     puts("4. Print the strings in order of the length of the first word in the string");
     puts("5. Quit");
 
-    while (scanf('%d', choice))    // use a while loop to feed user's choice
+    while (scanf("%d", choice))    // use a while loop to feed user's choice
     {                              // make a 5-choices structure with if-else statement
         if (choice == 1)           // call for different functions for each choice
             print_original_strings(array, COL);
@@ -48,6 +49,7 @@ int main(void)
         else if (choice == 5)
             break;
     }
+    puts("Done.");
     
     return 0;
 }
@@ -70,27 +72,84 @@ char *s_gets(char *st, int n)
     return ret_val;
 }
 
-int menu(void)
+void print_original_strings(const char st[][COL], int n)
 {
-
+    while (st)
+        puts(*st++);
 }
 
-void print_original_strings(const char st, int n)
+void print_ASCII_order(const char st[][COL], int n)
 {
+    int ct1 = 0;                // declare an integer as an index
+    
+    while (st[ct1])             // use a loop to sort the strings
+    {
+        char *small = st + ct1;  // declare a pointer pointing at the string of smallest ASCII code
+        int ct2 = 1;            // declare an integer as an index
 
+        while (st[ct1 + ct2])      
+        {
+            if (strncmp(st[ct1], st[ct1 + ct2], n) > 0) 
+                small = st + ct1 + ct2;
+            ct2++;
+        }
+        puts(*small);            // print the string.
+        ct1++;                  
+    }
 }
 
-void print_ASCII_order(const char st, int n)
+void print_increasing_length_order(const char st[][COL], int n)
 {
+    int ct1 = 0;                // declare an integer as an index
 
+    while (st[ct1++])             // use a loop to sort the strings
+    {
+        char *first = st + ct1; // declare a pointer pointing at the string of smallest ASCII code
+        int ct2 = 1;            // declare an integer as an index
+
+        while (st[ct1 + ct2])
+        {
+            if (strlen(st[ct1]) > strlen(st[ct1 + ct2]))
+                first = st + ct1 + ct2;
+            ct2++;
+        }
+        puts(*first);            // print the string.
+    }
 }
 
-void print_increasing_length_order(const char st, int n)
+void print_first_word_length_order(const char st[][COL], int n)
 {
+    int i = 0;
+    int len[10] = {0,0,0,0,0,0,0,0,0,0};
+    
+    while (st[i])
+    {
+        int j = 0;
+        int length = 0;
 
-}
+        while (isspace(st[i][j]))
+            j++;
 
-void print_first_word_length_order(const char st, int n)
-{
+        while (!isspace(st[i][j]))
+        {
+            length++;
+            j++;
+        }
+        len[i] = length;
+        i++;
+    }
 
+    int ct1 = 0;
+    while (len[ct1++])
+    {
+        int first = ct1;
+        int ct2 = 1;
+        while (len[ct1+ct2])
+        {
+            if (len[ct1] > len[ct1+ct2])
+                first = ct1 + ct2;
+            ct2++;
+        }
+        puts(st[first]);
+    }
 }
