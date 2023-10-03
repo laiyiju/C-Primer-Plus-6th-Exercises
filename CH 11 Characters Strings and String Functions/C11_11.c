8ff82b4bf7890a7b4d8aa8bc8b2c9f31c11c8596
頼以居 <137786746+laiyiju@users.noreply.github.com>
2023-10-03 16:41:39 +0900

    a function for sorting strings
    I spent a lot of time but still couldn't implement the functionality to read any number of strings below 10. So, in the end, I had to fix the number of strings to be read as 10.  Another issue is that I originally planned to use pointers for swapping the positions of strings, but I kept getting warnings during the compilation process. So, I had to resort to using the strncpy() function for swapping the positions of strings. I'm concerned that this might decrease the program's efficiency, but I don't know how to resolve this problem. I hope someone experienced can provide me with some insights on how to solve it.

--- CH 11 Characters Strings and String Functions/C11_11.c
+++ CH 11 Characters Strings and String Functions/C11_11.c
@@ -20,10 +20,10 @@ void print_first_word_length_order(char st[][COL], int n);
 
 int main(void)
 {
-    char array_origin[ROW][COL];  // declare an array of 10 strings,
+    char array_origin[ROW][COL];       // declare an array of 10 strings,
     char array_for_sorting[ROW][COL];
-    int i = 0;             // an integer as a counter,
-    int choice;            // and an integer to store user's choice.
+    int i = 0;                         // a counter,
+    int choice;                        // and a variable to store user's choice.
 
     puts("Enter 10 strings.(press Ctrl+Z to end program):");    // promtp for input
 
@@ -32,117 +32,137 @@ int main(void)
     {
         // Remove the newline character if present
         size_t len = strlen(array_origin[i]);
-        if (len > 0 && array_origin[i][len - 1] == '\n') {
-            array_origin[i][len - 1] = '\0';
-        }
+        if (len > 0 && array_origin[i][len - 1] == '\n') 
+        {
+            array_origin[i][len - 1] = '\0'; // replace '\n' with '\0' to mark the end of a string
+        }                                   
         i++;
     }
 
-    for (int j = 0; j < ROW; j++)       // duplicate the original strings
+    // duplicate the original strings for later use
+    for (int j = 0; j < ROW; j++)       
         strcpy(array_for_sorting[j], array_origin[j]);
 
-    puts("\nMake one choice from the menu:");     // print menu;
+    // print menu;
+    puts("\nMake one choice from the menu:");                  
     puts("  1. Print the original list of strings");
     puts("  2. Print the strings in ASCII collating sequence");
     puts("  3. Print the strings in order of increasing length");
     puts("  4. Print the strings in order of the length of the first word in the string");
     puts("  5. Quit");
 
-    while (scanf("%d", &choice))    // use a while loop to feed user's choice
-    {                               // make a 5-choices structure with if-else statement
+    // use a while loop to get user's choice
+    while (scanf("%d", &choice))    
+    {                              
         while (getchar() != '\n')
             continue;
-        if (choice == 1)            // call for different functions for each choice
-            print_original_strings(array_origin, i);
+        if (choice == 1)         
+            print_original_strings(array_origin, i);    // use original array
         else if (choice == 2)
-            print_ASCII_order(array_for_sorting, i);
-        else if (choice == 3)
+            print_ASCII_order(array_for_sorting, i);    // use duplicated array to prevent  
+        else if (choice == 3)                           // changes to the the original one
             print_increasing_length_order(array_for_sorting, i);
         else if (choice == 4)
             print_first_word_length_order(array_for_sorting, i);
         else if (choice == 5)
             break;
 
-        puts("\nNext choice(press 5 to quit):");
+        puts("\nNext choice(press 5 to quit):");        // prompt for the next input
     }
     puts("Done.");
     
     return 0;
 }
 
+// Function 1 (print the original list of strings)
 void print_original_strings(const char st[][COL], int n)
 {
     for (int i = 0; i < n; i++)
             puts(st[i]);
 }
 
+// Function 2 (print the strings in ASCII collating sequence)
 void print_ASCII_order(char st[][COL], int n)
 {
-    char tmp[40];
+    char tmp[40];                        // declare a string 
 
-    for (int i = 0; i < n; i++){ // use a loop to sort the strings
-        for (int j = 1; i + j < n; j++){
-            if (strncmp(st[i], st[i + j], COL) > 0){
-                strcpy(tmp, st[i]);
+    for (int i = 0; i < n; i++)          // outer loop to iterate through all the strings
+    {
+        for (int j = 1; i + j < n; j++)  // innter loop to iterate through all strings after st[i]
+        {
+            if (strncmp(st[i], st[i + j], COL) > 0)  // compare st[i] with the strings after it 
+            {
+                strcpy(tmp, st[i]);      // swap positions of st[i] and st[i + j]
                 strcpy(st[i], st[i + j]);
                 strcpy(st[i + j], tmp);
             }
         }
-        puts(st[i]);       // print the string.               
+        puts(st[i]);                     // print the string.               
     }
 }
 
+// Function 3 (print the strings in order of increasing length)
 void print_increasing_length_order(char st[][COL], int n)
 {
-    char tmp[40];                // declare a string array
+    char tmp[40];                       // declare a string array
 
-    for (int i = 0; i < n; i++){             // use a loop to sort the strings
-        for (int j = 1; i + j < n; j++){
-            if (strlen(st[i]) > strlen(st[i + j])){
+    for (int i = 0; i < n; i++)         // use an outer and an inner loop to sort the strings
+    {             
+        for (int j = 1; i + j < n; j++)
+        {
+            if (strlen(st[i]) > strlen(st[i + j]))
+            {
                 strcpy(tmp, st[i]);
                 strcpy(st[i], st[i + j]);
                 strcpy(st[i + j], tmp);
             }
         }
-        puts(st[i]);            // print the string.
+        puts(st[i]);                    // print the string.
     }
 }
 
+// Function 4 (print the strings in order of the length of the first word in the string)
 void print_first_word_length_order(char st[][COL], int n)
 {
-    int len[ROW] = {0,0,0,0,0,0,0,0,0,0};
-    
+    int len[ROW] = {0};                 // declare an integer string of ten '0's.    
+
+    // a loop to calculate the length of the first word in each string
     for (int i = 0; i < n; i++)
     {
         int j = 0;
         int length = 0;
 
-        while (isspace(st[i][j]))
-            j++;
+        while (isspace(st[i][j]))       // skip all whitespace characters at 
+            j++;                        // the beginning of a string, if any
 
         // the end of a string is a null character which is not a white-space
         // so not being '\0' should be added to the condition of the loop
         while (!isspace(st[i][j]) && st[i][j] != '\0')  
         {
-            length++;
-            j++;
+            length++;                   // count the length of the word
+            j++;                        // increment j to keep updating the loop
         }
-        len[i] = length;
+        len[i] = length;                // store the length into the declared array
     }
 
-    for (int g = 0; g < n; g++)
-        printf("%d ", len[g]);
-    puts("\n");
+    char tmp_st[COL];                   // declare a string array
+    int tmp_len;                        // declare an integer
 
-    char tmp[COL];                // declare a string array
-    for (int i = 0; i < n; i++){
-        for (int j = 1; i + j < n; j++){
-            if (len[i] > len[i + j]){
-                strncpy(tmp, st[i], COL);
-                strncpy(st[i], st[i + j], COL);
-                strncpy(st[i + j], tmp, COL);
+    // Sort strings based on the length stored in the array len[ROW]
+    for (int i = 0; i < n; i++)         
+    {
+        for (int j = 1; i + j < n; j++)
+        {
+            if (len[i] > len[i + j])
+            {
+                tmp_len = len[i];       // Swap lengths in len[]                         
+                len[i] = len[i + j];
+                len[i + j] = tmp_len;
+                strncpy(tmp_st, st[i], COL);     // swap strings in st[]
+                strncpy(st[i], st[i + j], COL);  
+                strncpy(st[i + j], tmp_st, COL); 
             }
         }
-        puts(st[i]);
+        puts(st[i]);                    // print the sorted strings
     }
 }
